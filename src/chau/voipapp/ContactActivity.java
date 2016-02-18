9d0b1cb3230c142d8478cb71db35fadd2a917422
tamchau2809 <tamchau2809@gmail.com>
2016-02-18 17:29:57 +0700

    update keyboard

--- src/chau/voipapp/ContactActivity.java
+++ src/chau/voipapp/ContactActivity.java
@@ -1,29 +1,56 @@
 package chau.voipapp;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class ContactActivity extends Fragment {
-
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.contact, menu);
-//		return true;
-//	}
-
+public class ContactActivity extends Fragment 
+{
+	View rootView;
+	
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
+	{	
+		setHasOptionsMenu(true);
+		rootView = inflater.inflate(R.layout.activity_history, container, false);
+		
+		initWiget();
+
+		
+		return rootView;
+	}
+	
+	@Override
+    public void onCreateOptionsMenu(Menu menu,MenuInflater inflater){
+        
+		inflater.inflate(R.menu.contact, menu);
+		
+        super.onCreateOptionsMenu(menu,inflater);
+        
+    }
+	
+	@Override
+	public void onPrepareOptionsMenu(Menu menu) 
+	{
+		
+		super.onPrepareOptionsMenu(menu);
+		getActivity().invalidateOptionsMenu();
+//		MenuItem filter = menu.findItem(R.id.bottomSearch).setVisible(false);
+//		filter = menu.findItem(R.id.bottomStatus).setVisible(false);
+//		filter.setVisible(false);
+		
+	}
+	
+	/**
+	 * Khởi tạo các thành phần
+	 */
+	public void initWiget()
+	{
+		
 	}
 }
